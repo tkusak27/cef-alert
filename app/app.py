8f7519204fd555fc96ae011268bd214940a6bd55
Tom Kusak <tkusak@nd.edu>
2024-08-11 21:42:47 -0500

    remove schedule monitor

--- app/app.py
+++ app/app.py
@@ -1,14 +1,6 @@
-from utils.schedule_monitor import has_run_today, update_last_run_date
 
 def main():
-    if not has_run_today():
-        # Your task here
-        print("Task running")
-
-        # Update the log to mark that the task has run today
-        update_last_run_date()
-    else:
-        print("Task has already run today.")
+    pass
 
 if __name__ == "__main__":
     main()
